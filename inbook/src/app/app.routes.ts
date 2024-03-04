084894c36ea59e63ef92458d3af656d25fd03f51
Lazzaro <lazzaro.carlucci@libero.it>
2024-03-04 16:17:23 +0100

    base app angular

--- inbook/src/app/app.routes.ts
+++ inbook/src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+   /* path: "",
+    // canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: "",
+        redirectTo: "login", // Modifica qui: da 'gestionale' a 'login'
+        pathMatch: "full",
+      },
+      {
+        path: "gestionale",
+        loadChildren: () =>
+          import("").then(
+            (m) => m.routes
+          ),
+      },
+      {
+        path: "user",
+        loadChildren: () =>
+          import("").then(
+            (m) => m.routes
+          ),
+      },
+      {
+        path: "login",
+        loadChildren: () =>
+          import("").then((m) => m.routes),
+      },
+    ],
+  */},
+  { path: "**", redirectTo: "login", pathMatch: "full" },
+];
--- inbook/src/main.ts
+++ inbook/src/main.ts
@@ -1,6 +1,41 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { LOCALE_ID, enableProdMode, importProvidersFrom } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TitleStrategy, provideRouter } from '@angular/router';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormGroupDirective } from '@angular/forms';
+import localeIt from '@angular/common/locales/it';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { Interceptor } from './app/modules/interceptor/interceptor';
+import { AngularMaterialModule } from './app/utils/material-module'
+ 
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+registerLocaleData(localeIt);
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true },
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'it-IT' },
+    { provide: FormGroupDirective },
+    //{ provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+    /*{
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },*/
+
+    importProvidersFrom(
+      BrowserModule,
+      HttpClientModule,
+      BrowserAnimationsModule
+    ),
+    importProvidersFrom(AngularMaterialModule),
+
+  ],
+}).catch((err) => console.error(err));
