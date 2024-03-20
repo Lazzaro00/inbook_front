fe1a72723330fc588ba6ea136ff56c760171ec38
GianlucaPatruno <gianluca.patruno@outlook.com>
2024-03-20 17:37:54 +0100

    library edit

--- src/app/modules/admin-profile/library-edit/library-edit.component.ts
+++ src/app/modules/admin-profile/library-edit/library-edit.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { LibraryService } from 'src/app/services/library.service';
 
 @Component({
   selector: 'app-library-edit',
@@ -8,8 +10,34 @@ import { RouterModule } from '@angular/router';
   templateUrl: './library-edit.component.html',
   styleUrls: ['./library-edit.component.scss'],
 })
-export class LibraryEditComponent {
+export class LibraryEditComponent{
+editing!:FormGroup;
+
+@Input()
+id:number=0;
 
 
+constructor(
+  private route: ActivatedRoute,
+  private fb:FormBuilder,
+  private libraryService: LibraryService,
+  private router:Router,
+  ) { 
+    this.libraryService.readLibrary(this.id).subscribe((res)=>
+    this.editing = this.fb.group({
+      id: this.id,
+      address: res.address,
+      province: res.province,
+      city: res.city,
+      description:res.description,
+      admins:res.admins
+    })
+    )
+  }
 
+save():void{
+  this.libraryService.updateLibrary(this.editing.value).subscribe({
+    next: () =>{this.router.navigate(["/gestionale/adminprofile"])},
+  })
+}
 }
